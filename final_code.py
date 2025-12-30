c6f909a216b8e8aba97fb602f863500ba2a7a81a
paul-bn <paul.benjamins@icloud.com>
2025-12-30 13:54:47 +0100

    final code

--- final_code.py
+++ final_code.py
@@ -22,45 +22,38 @@ df = pd.read_csv("factors.csv")
 df = df.drop(columns=(["Unnamed: 0","UMD_Europe","UMD_Global","UMD_US","D.Recession","P.Recession"]))
 df["RF"] = df["RF"] / 100
 
-###
 
-#einfaches Regressionsmodel
-X = sm.add_constant(df["Mkt_Europe"])   
-model = sm.OLS(df["BAB_Europe"],X).fit()
-Y = model.predict(X)
+#Deskriptive Statistik für Excel-Export, (Annualisierung erfolgte in Excel)
+res = df[["Mkt_Europe","BAB_Europe"]].describe().iloc[np.r_[1:3,4:7],:]
+res.loc["IQR"] = res.iloc[4] - res.iloc[2]
+res=res.rename(index={"50%":"median"})
+res = res.iloc[[3,5,0,1],:]
 
-model.summary()
+for col in res.columns:
+    res.loc["geometric_mean",col] = geometric_mean(1 + df[col]) - 1
 
+res[["BAB_Europe","Mkt_Europe"]]*100
 
-#Pearson Korrelationskoeffizeint
-rho = df["Mkt_Europe"].corr(df["BAB_Europe"], method="pearson")
-
-
-#Regressionsparamter für Excel-Export
-results_df = pd.DataFrame({
-    "coef": model.params,
-    "p_value": model.pvalues,
-    "r^2":model.rsquared,
-    "r^2 adjusted": model.rsquared_adj,
-    "pearson":rho,
-    "n":model.nobs
-    })
-results_df.to_excel("einfache reg.xlsx")
+res.to_excel("res.xlsx")
 
 
-#bp test, einfaches Regressionsmodell
-het_breuschpagan(model.resid, model.model.exog)
-    
+#boxplots
+fig, ax = plt.subplots(figsize=(10, 10))
+plt.style.use("seaborn-v0_8-whitegrid")
 
-#scatter plot und lineare Regressionsgerade
-plt.scatter(df["Mkt_Europe"],df["BAB_Europe"] , alpha=0.8, s = 7)
-plt.plot(df["Mkt_Europe"], Y, color="red")
-    
-plt.axhline(0, linewidth=1, color="black")
-plt.axvline(0, linewidth=1, color="black")
-plt.xlabel("Überschussrenditen des Marktportfolios")
-plt.ylabel("Überschussrenditen der BAB-Strategie")
-plt.tight_layout()
+ax.boxplot(
+    [df["BAB_Europe"], df["Mkt_Europe"]],
+    labels=["BAB-Strategie", "Marktportfolio"],
+    widths=0.3,
+    patch_artist=False,        
+    boxprops=dict(color="black", linewidth=1.5),
+    medianprops=dict(color="red", linewidth=2),  
+    whiskerprops=dict(color="black", linewidth=1.2),
+    capprops=dict(color="black", linewidth=1.2),
+    flierprops=dict(marker='o', markersize=4, markerfacecolor='black', markeredgecolor='black')
+)
+ax.tick_params(axis='both', labelsize=20)
+ax.axhline(0, color='black', linewidth=1)
 plt.show()
 
 
@@ -78,6 +71,7 @@ plt.title("Marktportfolio")
 plt.xlabel("Theoretische Quantile der Normalverteilung")
 plt.ylabel("Empirische Quantile des Datensamples")
 plt.show()
+
 
 #Kurtosis nach Fisher
 kurtosis(df["BAB_Europe"], fisher=True)
@@ -117,45 +111,50 @@ wilcoxon(df["BAB_Europe"]-df["Mkt_Europe"],alternative="greater")[1]
 stats.ttest_1samp(df["BAB_Europe"]-df["Mkt_Europe"], popmean=0,alternative="greater")[1]
 
 
-#Deskriptive Statistik für Excel-Export, (Annualisierung erfolgte in Excel)
-res = df[["Mkt_Europe","BAB_Europe"]].describe().iloc[np.r_[1:3,4:7],:]
-res.loc["IQR"] = res.iloc[4] - res.iloc[2]
-res=res.rename(index={"50%":"median"})
-res = res.iloc[[3,5,0,1],:]
-
-for col in res.columns:
-    res.loc["geometric_mean",col] = geometric_mean(1 + df[col]) - 1
-
-res[["BAB_Europe","Mkt_Europe"]]*100
-
-res.to_excel("res.xlsx")
-
-
-#boxplots
-fig, ax = plt.subplots(figsize=(10, 10))
-plt.style.use("seaborn-v0_8-whitegrid")
-
-ax.boxplot(
-    [df["BAB_Europe"], df["Mkt_Europe"]],
-    labels=["BAB-Strategie", "Marktportfolio"],
-    widths=0.3,
-    patch_artist=False,        
-    boxprops=dict(color="black", linewidth=1.5),
-    medianprops=dict(color="red", linewidth=2),  
-    whiskerprops=dict(color="black", linewidth=1.2),
-    capprops=dict(color="black", linewidth=1.2),
-    flierprops=dict(marker='o', markersize=4, markerfacecolor='black', markeredgecolor='black')
-)
-
-ax.tick_params(axis='both', labelsize=20)
-ax.axhline(0, color='black', linewidth=1)
-plt.show()
-
-
 #Annualisierte Sharpe Ratio
 qs.extend_pandas()
 df["BAB_Europe"].sharpe(periods=12)
 df["Mkt_Europe"].sharpe(periods=12)
+
+
+#einfaches Regressionsmodel
+X = sm.add_constant(df["Mkt_Europe"])   
+model = sm.OLS(df["BAB_Europe"],X).fit()
+Y = model.predict(X)
+
+model.summary()
+
+
+#Pearson Korrelationskoeffizient
+rho = df["Mkt_Europe"].corr(df["BAB_Europe"], method="pearson")
+
+
+#Regressionsparamter für Excel-Export
+results_df = pd.DataFrame({
+    "coef": model.params,
+    "p_value": model.pvalues,
+    "r^2":model.rsquared,
+    "r^2 adjusted": model.rsquared_adj,
+    "pearson":rho,
+    "n":model.nobs
+    })
+results_df.to_excel("einfache reg.xlsx")
+
+
+#bp test, einfaches Regressionsmodell
+het_breuschpagan(model.resid, model.model.exog)
+    
+
+#scatter plot und lineare Regressionsgerade
+plt.scatter(df["Mkt_Europe"],df["BAB_Europe"] , alpha=0.8, s = 7)
+plt.plot(df["Mkt_Europe"], Y, color="red")
+    
+plt.axhline(0, linewidth=1, color="black")
+plt.axvline(0, linewidth=1, color="black")
+plt.xlabel("Überschussrenditen des Marktportfolios")
+plt.ylabel("Überschussrenditen der BAB-Strategie")
+plt.tight_layout()
+plt.show()
 
 
 #Multiples Regressionsmodel (Fama French 3-Faktor Modell)
@@ -165,13 +164,11 @@ ff_model.summary()
 
 
 #Regressionsparamter des multiplen Regressionsmodells für Excel-Export
-R = np.sqrt(ff_model.rsquared)
 results_ff_df = pd.DataFrame({
     "coef": ff_model.params,
     "p_value": ff_model.pvalues,
     "r^2": ff_model.rsquared,
     "r^2 adj.":ff_model.rsquared_adj, 
-    "Wurzel aus Bestimmheitsmaß": R,
     "n": ff_model.nobs
 })
 results_ff_df.to_excel("multiple reg.xlsx")
@@ -186,25 +183,20 @@ X = df[["Mkt_Europe","SMB_Europe","HML_Europe"]]
 vif = pd.Series(
     [variance_inflation_factor(X.values, i) for i in range(X.shape[1])],
     index=X.columns)
-print(vif)
 
 
 #Equity Curve
 final = pd.DataFrame()
-
 final["BAB_total_return"] = df["BAB_Europe"] + df["RF"]
 final["Mkt_total_return"] = df["Mkt_Europe"] + df["RF"]
-    
 final["BAB"] = (1+final["BAB_total_return"]).cumprod().to_frame()
 final["Mkt"] = (1+final["Mkt_total_return"]).cumprod().to_frame()
-
 final["time"] = df["mdate"]
 final = final.set_index("time")
 
 plt.plot(final.index.astype(str), final[["BAB","Mkt"]].values, label=["BAB","Market"])
 plt.gca().xaxis.set_major_locator(plt.MaxNLocator(16))
 plt.xticks(rotation=60)
-
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
